f435d2a29f6f464bd80842ce637d11bc63f1706b
Mahaswi <tanmaimaha0903@gmail.com>
2026-08-31 22:47:31 +0530

    1A,C,D

--- pa1-dhurandhar-microarchitecture-main/task1/src/conv_reorder.cpp
+++ pa1-dhurandhar-microarchitecture-main/task1/src/conv_reorder.cpp
@@ -2,9 +2,56 @@
 // Hint: loops from outermost to innermost -> ky, kx, oy, ox.
 
 #include "convolution.h"
+#include<iostream>
 
 void conv_reorder(const float* in, float* out, const float* ker,
                   int H, int W, int K) {
-    // TODO(student): replace this placeholder with your reordered implementation.
-    conv_naive(in, out, ker, H, W, K);
+   
+    const int p = K / 2;
+    const int in_stride = W + 2 * p;  // padded row stride
+
+    // for (int oy = 0; oy < H; ++oy) {
+    //     for (int ox = 0; ox < W; ++ox) {
+    //         float acc = 0.0f;
+    //         for (int ky = 0; ky < K; ++ky) {
+    //             for (int kx = 0; kx < K; ++kx) {
+    //                 acc += in[(oy + ky) * in_stride + (ox + kx)] * ker[ky * K + kx];
+    //             }
+    //         }
+    //         out[oy * W + ox] = acc;
+    //     }
+    // }
+    float acc = 0.0f;
+    int ad2=0;
+    for(int oy=0;oy<H;oy++){
+        ad2=(oy*W);
+            for(int ox=0;ox<W;ox++){
+                out[ad2 + ox]=acc;
+            }
+        }
+    ad2=0;
+    int addi=0;
+    int indi=0;
+    int addi2=0;
+    int addi3=0;
+    int indi2=0;
+    for(int ky=0;ky<K;++ky){
+        
+        for(int kx=0;kx<K;++kx){     
+            addi2=addi+kx;
+            indi2=indi+kx;
+            addi3=addi2;
+            ad2=0;
+            for(int oy=0;oy<H;oy++){
+
+                for(int ox=0;ox<W;ox++){
+                    out[ad2 + ox]+=in[addi3+ox] * ker[indi2];
+                }
+                addi3+=in_stride;
+                ad2+=W;
+            }
+        }
+        addi+=in_stride;
+        indi+=K;
+    }
 }
--- pa1-dhurandhar-microarchitecture-main/task1/src/simd+trail.cpp
+++ pa1-dhurandhar-microarchitecture-main/task1/src/simd+trail.cpp
@@ -12,28 +12,41 @@ void conv_optimized(const float* in, float* out, const float* ker,
     const int p = K / 2;
     const int in_stride = W + 2 * p;  // padded row stride
 
-    for (int oy = 0; oy < H; ++oy) {
-        int ox=0;
-        for (; ox+15 < W; ox+=16) {
+    // for (int oy = 0; oy < H; ++oy) {
+    //     for (int ox = 0; ox < W; ++ox) {
+    //         float acc = 0.0f;
+    //         for (int ky = 0; ky < K; ++ky) {
+    //             for (int kx = 0; kx < K; ++kx) {
+    //                 acc += in[(oy + ky) * in_stride + (ox + kx)] * ker[ky * K + kx];
+    //             }
+    //         }
+    //         out[oy * W + ox] = acc;
+    //     }
+    // }
+    int tile_size=64;
+    for(int i=0;i<H;i+=tile_size){
+        for(int j=0;j<W;j+=tile_size){
+            int i_end=(i+tile_size < H)?i+tile_size :H;
+            int j_end=(j+tile_size< W)? j+tile_size : W;
+    
+    for (int oy = i; oy < i_end; ++oy) {
+        int ox=j;
+        for (; ox+3 < j_end; ox+=4) {
             
             __m256 vacc=_mm256_setzero_ps();
-            __m256 vacc1=_mm256_setzero_ps();
             for (int ky = 0; ky < K; ++ky) {
                 int indi=((oy+ky)*in_stride)+ox;
                 for (int kx = 0; kx < K; ++kx) {
                     __m256 vin  = _mm256_loadu_ps(&in[indi+kx]);
                     __m256 vker = _mm256_set1_ps(ker[ky * K + kx]);
-                    __m256 vin1  = _mm256_loadu_ps(&in[indi+kx+8]);
                     vacc=_mm256_fmadd_ps(vin,vker,vacc);
-                    vacc1=_mm256_fmadd_ps(vin1,vker,vacc1);
                 }
                 
             }
              _mm256_storeu_ps(&out[oy * W + ox], vacc);
-             _mm256_storeu_ps(&out[oy * W + ox + 8], vacc1);
            
         }
-        for (; ox < W; ++ox) {
+        for (; ox < j_end; ++ox) {
             float acc = 0.0f;
             for (int ky = 0; ky < K; ++ky) {
                 for (int kx = 0; kx < K; ++kx) {
@@ -43,4 +56,7 @@ void conv_optimized(const float* in, float* out, const float* ker,
             out[oy * W + ox] = acc;
         }
     }
+        }
+    }
+  
 }
